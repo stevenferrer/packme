ac4c16f47074e20191027ceb402c01cb44195e2d
Steven Ferrer <steven.r.ferrer@gmail.com>
2021-07-19 11:55:56 +0800

    Move all related packing logic to BestFitPacker

--- best_fit_packer.go
+++ best_fit_packer.go
@@ -35,7 +35,7 @@ func (bfp BestFitPacker) Pack(boxSpecs []BoxSpec, itemSpecs []ItemSpec) PackingS
 
 			// Try packing item to existing boxes
 			for _, box := range boxes {
-				packed = bfp.packToBox(box, newItem)
+				packed = bfp.pack(box, newItem)
 				if packed {
 					// Item was packed, we're done
 					break
@@ -52,7 +52,7 @@ func (bfp BestFitPacker) Pack(boxSpecs []BoxSpec, itemSpecs []ItemSpec) PackingS
 						box := NewBox(bxSpec.desc, bxSpec.dims)
 
 						// See if item fits in the new box
-						packed = bfp.packToBox(box, newItem)
+						packed = bfp.pack(box, newItem)
 						if packed {
 							// Reduce number of available boxes
 							boxSpecs[i].qty--
@@ -74,10 +74,10 @@ func (bfp BestFitPacker) Pack(boxSpecs []BoxSpec, itemSpecs []ItemSpec) PackingS
 	return PackingScheme{Boxes: boxes, NotPacked: notPacked}
 }
 
-func (bfp BestFitPacker) packToBox(box *Box, newItem *Item) bool {
+func (bfp BestFitPacker) pack(box *Box, newItem *Item) bool {
 	// Box doesn't have any items yet
 	if len(box.items) < 1 {
-		return box.packItem(newItem, NewPoint(0, 0, 0))
+		return bfp.packToBox(box, newItem, NewPoint(0, 0, 0))
 	}
 
 	// Try possible placements for the new item
@@ -87,11 +87,60 @@ func (bfp BestFitPacker) packToBox(box *Box, newItem *Item) bool {
 			// Compute pivot point
 			pivot := computePivot(axis, boxItem.pos, boxItem.Dimensions())
 			// Try packing the item to the pivot point
-			if box.packItem(newItem, pivot) {
+			if bfp.packToBox(box, newItem, pivot) {
 				return true
 			}
 		}
 	}
 
 	return false
+}
+
+func (bfp BestFitPacker) packToBox(bx *Box, item *Item, pivot Point) bool {
+	var packed bool
+
+	// Set item position to pivot
+	item.pos = pivot
+	// Try posible packing rotations
+	for rot := RotationLWH; rot <= RotationLHW; rot++ {
+		// Set item rotation
+		item.rot = rot
+		// Get item dimensions based on current rotatoin
+		idims := item.Dimensions()
+
+		// Box dimensions must be able accomodate
+		// the item in current rotation
+		if bx.dims.Length() < pivot.X()+idims.Length() ||
+			bx.dims.Width() < pivot.Y()+idims.Width() ||
+			bx.dims.Height() < pivot.Z()+idims.Height() {
+			continue
+		}
+		// Assume item is packed
+		packed = true
+
+		// See if current item might collide with other
+		// items inside the box
+		for _, i := range bx.items {
+			if i.Collision(item) {
+				// Can't pack coz it collides with another item
+				packed = false
+				break
+			}
+		}
+
+		// Item is packed
+		if packed {
+			// Add item to the box
+			bx.items = append(bx.items, item)
+		}
+
+		break
+	}
+
+	if !packed {
+		// Reset item position
+		item.pos = NewPoint(0, 0, 0)
+	}
+
+	return packed
 }
--- box.go
+++ box.go
@@ -35,55 +35,6 @@ func (bx *Box) Items() Items {
 	return bx.items[:]
 }
 
-func (bx *Box) packItem(item *Item, pivot Point) bool {
-	var packed bool
-
-	// Set item position to pivot
-	item.pos = pivot
-	// Try posible packing rotations
-	for rot := RotationLWH; rot <= RotationLHW; rot++ {
-		// Set item rotation
-		item.rot = rot
-		// Get item dimensions based on current rotatoin
-		idims := item.Dimensions()
-
-		// Box dimensions must be able accomodate
-		// the item in current rotation
-		if bx.dims.Length() < pivot.X()+idims.Length() ||
-			bx.dims.Width() < pivot.Y()+idims.Width() ||
-			bx.dims.Height() < pivot.Z()+idims.Height() {
-			continue
-		}
-		// Assume item is packed
-		packed = true
-
-		// See if current item might collide with other
-		// items inside the box
-		for _, i := range bx.items {
-			if i.Collision(item) {
-				// Can't pack coz it collides with another item
-				packed = false
-				break
-			}
-		}
-
-		// Item is packed
-		if packed {
-			// Add item to the box
-			bx.items = append(bx.items, item)
-		}
-
-		break
-	}
-
-	if !packed {
-		// Reset item position
-		item.pos = NewPoint(0, 0, 0)
-	}
-
-	return packed
-}
-
 func (bx *Box) String() string {
 	buf := &bytes.Buffer{}
 	buf.WriteString(fmt.Sprintf("%s items(%v) %s vol(%v)",
